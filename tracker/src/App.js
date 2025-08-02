43c2e9225f66cf56528d2843386f269d88ae329e
Yuxin Chen <sakura12217878@gmail.com>
2025-08-01 23:47:04 -0400

    starter

--- tracker/src/App.js
+++ tracker/src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import LoginPage from './pages/LoginPage';
+import DashboardPage from './pages/DashboardPage';
+import CalendarPage from './pages/CalendarPage';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<LoginPage />} />
+        <Route path="/dashboard" element={<DashboardPage />} />
+        <Route path="/calendar" element={<CalendarPage />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ tracker/src/pages/CalendarPage.js
@@ -0,0 +1,3 @@
+export default function CalendarPage() {
+  return <h1>Calendar Page</h1>;
+}
