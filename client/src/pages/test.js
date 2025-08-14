20bee9a0aa85a21c535fa4398a8c8878874cc17d
TJTforever <156537906+TJTforever@users.noreply.github.com>
2025-08-14 13:04:49 -0400

    WIP

---
+++ client/src/pages/test.js
@@ -0,0 +1,60 @@
+import React, { useEffect, useState } from 'react'
+//import './App.css'
+import { gapi } from 'gapi-script'
+import Event from '../Components/Event.js'
+
+function Test() {
+    const [events, setEvents] = useState([])
+
+    const calendarID = process.env.REACT_APP_CALENDAR_ID
+    const apiKey = process.env.REACT_APP_GOOGLE_API_KEY
+    const accessToken = process.env.REACT_APP_GOOGLE_ACCESS_TOKEN
+
+    //console.log(process.env.REACT_APP_CALENDAR_ID + " :ci ak: " + process.env.REACT_APP_GOOGLE_API_KEY + " at: " + process.env.REACT_APP_GOOGLE_ACCESS_TOKEN);
+
+    const getEvents = (calendarID, apiKey) => {
+        function initiate() {
+            gapi.client
+                .init({
+                    apiKey: apiKey,
+                })
+                .then(function () {
+                    return gapi.client.request({
+                        path: `https://www.googleapis.com/calendar/v3/calendars/${calendarID}/events`,
+                    })
+                })
+                .then(
+                    (response) => {
+                        let events = response.result.items
+                        setEvents(events)
+                    },
+                    function (err) {
+                        return [false, err]
+                    }
+                )
+        }
+        gapi.load('client', initiate)
+    }
+
+    useEffect(() => {
+        const events = getEvents(calendarID, apiKey)
+        setEvents(events)
+    }, [])
+
+    return (
+        <div className="App flex flex-col justify-center py-8">
+            <h1 className="mb-4 text-2xl font-bold">
+                React App with Google Calendar API!
+                <ul>
+                    {events?.map((event) => (
+                        <li key={event.id} className="flex justify-center">
+                            <Event description={event.summary} />
+                        </li>
+                    ))}
+                </ul>
+            </h1>
+        </div>
+    )
+}
+
+export default Test
