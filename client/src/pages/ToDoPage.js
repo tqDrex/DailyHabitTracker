762476037bc380216fe2c6a79aef6b763dbb63c3
tqDrex <ts3375@drexel.edu>
2025-08-17 00:40:25 -0400

    updated todo

--- client/src/pages/ToDoPage.js
+++ client/src/pages/ToDoPage.js
@@ -1,10 +1,25 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import '../style/ToDoPage.css';
 
+const API = 'http://localhost:3000';
+
 export default function ToDoPage() {
-  const [rows, setRows] = useState([
-    { id: 1, name: "Item 1" }
-  ]);
+const [rows, setRows] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    fetch(`${API}/`)
+    fetch(`${API}/tasks`, { credentials: "include" })
+      .then((res) => res.json())
+      .then((data) => {
+        setRows(data.rows);
+        setLoading(false);
+      })
+      .catch((err) => {
+        console.error("Error fetching tasks:", err);
+        setLoading(false);
+      })
+  });
 
   const addRow = () => {
     const newId = rows.length + 1;
@@ -17,6 +32,8 @@ export default function ToDoPage() {
     addRow();
   };
 
+  if (loading) return <p>Loading Tasks...</p>;
+
   return (
     <div>
       <h1>To-Do Page</h1>
@@ -25,15 +42,21 @@ export default function ToDoPage() {
           <table border="1" cellPadding="10" style={{borderCollapse: 'collapse'}}>
             <thead>
               <tr>
-                <th>ID</th>
-                <th>Name</th>
+                <th>Activity Name</th>
+                <th>Timer</th>
+                <th>Counter</th>
+                <th>Deadline Date</th>
+                <th>Repeat</th>
               </tr>
             </thead>
             <tbody>
               {rows.map(row => (
                 <tr key={row.id}>
-                  <td>{row.id}</td>
-                  <td>{row.name}</td>
+                  <td>{row.activity_name}</td>
+                  <td>{row.timer}</td>
+                  <td>{row.counter}</td>
+                  <td>{row.deadline_date}</td>
+                  <td>{row.repeat}</td>
                 </tr>
               ))}
             </tbody>
